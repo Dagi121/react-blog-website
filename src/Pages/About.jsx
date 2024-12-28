5fe6e54ce9ca81eb6841c10f152200b7c79a60ef
Dagim sime <149010325+Dagi121@users.noreply.github.com>
2024-12-28 14:15:48 +0200

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div>
      <div className='py-40 bg-black text-white text-center px-4'>
-      <h2 className='text-5xl lg:text-7xl leading-snug font-bold mb-5'>About us Page</h2>
+      <h2 className='text-5xl lg:text-7xl leading-snug font-bold mb-5'>About us Dagim Sime Page</h2>
       </div>
     </div>
   )
